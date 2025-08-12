eb34e923c5429f5d6f01a22c714156359e285f5f
LimaDev <lbm9999@proton.me>
2025-08-12 16:02:10 -0300

    Delete ex04.py

--- ex04.py
+++
@@ -1,8 +0,0 @@
-distancia = float(input("Digite a distância da viagem em Km: "))
-
-if distancia <= 200:
-    preco = distancia * 0.50
-else:
-    preco = distancia * 0.45
-
-print(f"O preço da passagem é: R${preco:.2f}")
