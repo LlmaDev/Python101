eb704d9bd6edb8ce5530c68d3dcbc08aee18083a
LimaDev <lbm9999@proton.me>
2025-08-12 16:01:43 -0300

    Delete ex02.py

--- ex02.py
+++
@@ -1,9 +0,0 @@
-numero = int(input("Digite o número que deseja ver a tabuada: "))
-
-inicio = int(input("Digite o início do intervalo: "))
-fim = int(input("Digite o fim do intervalo: "))
-
-print(f"\n📌 Tabuada do {numero} de {inicio} até {fim}:\n")
-
-for i in range(inicio, fim + 1):
-    print(f"{numero} x {i} = {numero * i}")
