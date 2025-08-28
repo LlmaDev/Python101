9880ff682847b9b2c595c79c519b515d732b9e4f
LimaDev <lbm9999@proton.me>
2025-08-28 15:28:56 -0300

    Delete Cap.4-Numpy/class directory

--- Cap.4-Numpy/class/spaceDatabase.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-database = np.loadtxt('space.csv', delimiter = ';', dtype = str, encoding = 'utf-8');
-##print(database);
-cost = database[:,6];
-costf = cost.astype(float);
-print(np.sum((cost.astype(float))));
